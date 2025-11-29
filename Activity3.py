f188dde13f95667fe4921bf7ef297d2f6f8027d2
Aayansh889 <aayansh.saxena@gmail.com>
2025-11-29 14:45:16 +0530

    Add files via upload

---
+++ Activity3.py
@@ -0,0 +1,25 @@
+a=10
+b=12
+c=12
+
+print(a != b)
+print(b != c)
+
+x="python"
+y="coding"
+
+if x != y:
+    print(x,"and",y,"are different")
+
+m=4
+n=5
+
+if (m==1) != (n==5):
+    print("Hello")
+
+z=int(input("enter a number:"))
+
+if z%2 != 0:
+    print(z, "is odd")
+else:
+    print(z, "is even")
